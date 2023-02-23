2f4a358ecf5815e1323714ef2026a7942ea19b48
Mert Bayats <mertbaytas@gmail.com>
2023-02-23 13:57:31 +0300

    basla

--- src/CevreAlanHesaplama.java
+++ src/CevreAlanHesaplama.java
@@ -3,17 +3,29 @@ import java.lang.Math;
 public class CevreAlanHesaplama {
 
     public static void main(String[] args) {
-        double kenar1,kenar2,hipotenus;
+        int k1;
+        int k2;
+        int k3;
+        double u,alan,cevre;
         Scanner oku=new Scanner(System.in);
 
-        System.out.println("Ucgenin dik olmayan ilk kenarini giriniz: ");
-        kenar1=oku.nextInt();
+        System.out.println("Ucgenin ilk kenarini giriniz: ");
+        k1=oku.nextInt();
 
-        System.out.println("Ucgenin dik olmayan ikinci kenarini giriniz: ");
-        kenar2=oku.nextInt();
+        System.out.println("Ucgenin ikinci kenarini giriniz: ");
+        k2=oku.nextInt();
 
-        hipotenus=Math.sqrt(kenar1*kenar1+kenar2*kenar2);
-        System.out.println("Ucgenin uzun kenari = "+hipotenus+" !");
+        System.out.println("Ucgenin ucuncu kenarini giriniz: ");
+        k3=oku.nextInt();
+
+        u=(k1+k2+k3)/2;
+        cevre=2*u;
+        alan=Math.sqrt(u*(u-k1)*(u-k2)*(u-k3));
+
+        System.out.println("Cevre = "+cevre+"\nAlan= "+alan);
+
+
+
 
 
 
--- src/HipotenusHesaplama.java
+++ src/HipotenusHesaplama.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class HipotenusHesaplama {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double kenar1,kenar2,hipotenus;
+        Scanner oku=new Scanner(System.in);
+
+        System.out.println("Ucgenin dik olmayan ilk kenarini giriniz: ");
+        kenar1=oku.nextInt();
+
+        System.out.println("Ucgenin dik olmayan ikinci kenarini giriniz: ");
+        kenar2=oku.nextInt();
+
+        hipotenus=Math.sqrt(kenar1*kenar1+kenar2*kenar2);
+        System.out.println("Ucgenin uzun kenari = "+hipotenus+" !");
+
+
+
     }
 }
