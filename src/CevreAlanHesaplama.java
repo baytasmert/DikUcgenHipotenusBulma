57d9e3d423492d00da9ebd0efcfd6d853cbb62c1
Mert Bayats <mertbaytas@gmail.com>
2023-02-23 13:49:12 +0300

    basla

---
+++ src/CevreAlanHesaplama.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class CevreAlanHesaplama {
+
+    public static void main(String[] args) {
+        double kenar1,kenar2,hipotenus;
+        Scanner oku=new Scanner(System.in);
+
+        System.out.println("Ucgenin dik olmayan ilk kenarini giriniz: ");
+        kenar1=oku.nextInt();
+
+        System.out.println("Ucgenin dik olmayan ikinci kenarini giriniz: ");
+        kenar2=oku.nextInt();
+
+        hipotenus=Math.sqrt(kenar1*kenar1+kenar2*kenar2);
+        System.out.println("Ucgenin uzun kenari = "+hipotenus+" !");
+
+
+
+    }
+}
--- src/HipotenusHesaplama.java
+++ src/HipotenusHesaplama.java
@@ -1,4 +1,4 @@
-public class Main {
+public class HipotenusHesaplama {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
